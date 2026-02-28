b878502507f011b3e0f7fcfa74894e3da8487de8
repr <repr-man@noreply.codeberg.org>
2026-02-28 17:23:25 -0600

    Added loading state to RecommendationCard

--- frontend/src/components/RecommendationCard.tsx
+++ frontend/src/components/RecommendationCard.tsx
@@ -4,6 +4,7 @@ import BowlWhisk from "@/assets/bowl-whisk.svg";
 import { RecipeCardFooter } from "@/components/RecipeCard";
 import { useRecipe } from "@/hooks/queries";
 import EditRecipeDialogProvider from "./EditRecipeDialogProvider";
+import { Skeleton } from "./ui/skeleton";
 
 export default function RecommendationCard({
   recipeId,
@@ -12,7 +13,8 @@ export default function RecommendationCard({
   recipeId: number;
   orientation: boolean;
 }) {
-  const recipe = useRecipe(recipeId).data!;
+  const { data: recipe, isLoading } = useRecipe(recipeId);
+  if (isLoading || !recipe) return <RecommendationCardSkeleton />;
   return (
     <EditRecipeDialogProvider recipeId={recipeId}>
       <Card
@@ -55,5 +57,27 @@ export default function RecommendationCard({
         </div>
       </Card>
     </EditRecipeDialogProvider>
+  );
+}
+
+function RecommendationCardSkeleton() {
+  return (
+    <Card className={`flex flex-row w-2/3 p-0 overflow-hidden`}>
+      <Skeleton className={`w-2/5 object-cover shrink-0 h-72 rounded-r-none`} />
+      <div className="flex flex-col w-3/5 py-4 gap-2">
+        <CardTitle className="px-6 font-bold select-none line-clamp-1">
+          <Skeleton className="w-1/2 h-6" />
+        </CardTitle>
+        <div className="relative flex-1 overflow-hidden px-6">
+          <CardDescription className="select-none">
+            <div className="flex flex-col gap-2">
+              <Skeleton className="w-3/4 h-4" />
+              <Skeleton className="w-3/4 h-4" />
+              <Skeleton className="w-1/4 h-4" />
+            </div>
+          </CardDescription>
+        </div>
+      </div>
+    </Card>
   );
 }
